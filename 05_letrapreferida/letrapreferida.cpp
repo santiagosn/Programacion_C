929f25fb9b741030356dc28fc2681891dff35a91
santiagosn <santiagosieger.ciclos@gmail.com>
2016-12-30 19:17:40 +0100

    Trabajo Navidad

---
+++ 05_letrapreferida/letrapreferida.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(){
+
+ char  letrapreferida;
+
+ printf("Letra preferida: ");
+ scanf(" %c", &letrapreferida);
+
+ return EXIT_SUCCESS;
+
+} 
+
+
+
+
