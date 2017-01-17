52694791421aa6f71e27bac7a4154697721c90fd
santiagosn <santiagosieger.ciclos@gmail.com>
2017-01-17 16:27:55 +0100

    Trabajo Navidad

---
+++ 017_arrays_cuadradonaturales/cuadradonaturales.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+int cuadrado[11];
+
+for( int num = 1; num < 11; num++){
+
+cuadrado[num] = num * num;
+
+printf("%i\n", cuadrado[num]);
+
+} 
+ return EXIT_SUCCESS;
+}
+
+ 
